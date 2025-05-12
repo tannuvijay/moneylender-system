a371b52eca528d186981280a62b212375d8a51e3
Tannu Vijay <2023ucp1620@mnit.ac.in>
2025-05-12 18:30:59 +0530

    second commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 const Owner = require('../models/Owner');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 exports.signup = async (req, res) => {
   const { name, number, password } = req.body;
